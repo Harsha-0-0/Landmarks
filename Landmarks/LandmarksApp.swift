dbf2b44ef30288d7403bd29e493c6785f55c9334
Harsha <harshiismyname@gmail.com>
2025-03-20 14:14:59 +1100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Harshi on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
